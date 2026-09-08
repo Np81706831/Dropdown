d58df52c3709cd43d347b4c1df7b33748e21878f
Narges Poorhashemi <nargespoorhashemi2@gmail.com>
2026-09-08 17:58:09 +0330

    add readme file

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*................this is for DropDown........... */
 const menuButton1=document.getElementById("menu-button1");
 const menuButton2=document.getElementById("menu-button2");
 const menu1=document.getElementById("menu1");
@@ -51,5 +52,59 @@ button3.addEventListener("click",function ()
     mohtava2.classList.remove("active");
     mohtava3.classList.remove("active");
     mohtava3.classList.add("active");
+}
+)
+
+/*...................this is for modal.......................*/
+const readmore1=document.querySelector("#readmore1");
+const readmore2=document.querySelector("#readmore2");
+const readmore3=document.querySelector("#readmore3");
+
+const modal1=document.querySelector("#modal1");
+const modal2=document.querySelector("#modal2");
+const modal3=document.querySelector("#modal3");
+
+const cancel1=document.querySelector("#cancel1");
+const cancel2=document.querySelector("#cancel2");
+const cancel3=document.querySelector("#cancel3");
+
+readmore1.addEventListener("click",function()
+{
+    modal1.classList.toggle("show");
+    cancel1.classList.toggle("active");
+}
+)
+cancel1.addEventListener("click" , function()
+{
+    modal1.classList.toggle("show");
+    cancel1.classList.toggle("active");
+}
+)
+
+readmore2.addEventListener("click",function()
+{
+    modal2.classList.toggle("show");
+    cancel2.classList.toggle("active");
+}
+)
+cancel2.addEventListener("click" , function()
+{
+    modal2.classList.toggle("show");
+    cancel2.classList.toggle("active");
+
+}
+)
+
+readmore3.addEventListener("click",function()
+{
+    modal3.classList.toggle("show");
+    cancel3.classList.toggle("active");
+}
+)
+cancel3.addEventListener("click" , function()
+{
+    modal3.classList.toggle("show");
+    cancel3.classList.toggle("active");
+
 }
 )
